f90804e24551d6e99893a19471e1ccb41b048848
Ibrahim Azam <ibazam@microsoft.com>
2021-10-27 18:06:21 +0300

    new

--- SciKit-Learn/1.3 Metrics Module/1.3.4.2.py
+++ SciKit-Learn/1.3 Metrics Module/1.3.4.2.py
@@ -1,8 +1,12 @@
 from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 y_pred = ['a','a','b','b','a','b','a','a','a','a']
 y_true  = ['a','b','b','a','b','a','a','b','a','b']
-confusion_matrix(y_true, y_pred)
-
+CM= confusion_matrix(y_true, y_pred)
+# drawing confusion matrix
+sns.heatmap(CM, center = True)
+plt.show()
 #=======================================================================
 
 y_pred = ['a','b','c','a','b','c','a','b','c','a']
@@ -13,5 +17,8 @@ confusion_matrix(y_true, y_pred)
 
 
 y_pred = [5,8,9,9,8,5,5,9,8,5,9,8]
-y_true =  [9,9,8,8,5,5,9,5,8,9,8,5]
-confusion_matrix(y_true, y_pred)
+y_true =  [9,9,8,8,5,5,9,5,8,9,8,5]=confusion_matrix(y_true, y_pred)
+
+# drawing confusion matrix
+sns.heatmap(CM, center = True)
+plt.show()
